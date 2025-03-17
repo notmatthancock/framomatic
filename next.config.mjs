357b0353c36b1e8c241a7dc921d84478affa7fa6
Matt Hancock <not.matt.hancock@gmail.com>
2025-03-16 21:34:18 -0500

    Add url prefix to build

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,18 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const data = require("./package.json");
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-      config.resolve.fallback = { fs: false };
-      return config;
-    },
-    publicRuntimeConfig: {
-      appVersion: data.version
-    },
-    distDir: 'build',
-    output: 'export',
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+  publicRuntimeConfig: {
+    appVersion: data.version,
+  },
+  basePath: process.env?.URL_PREFIX,
+  distDir: "build",
+  output: "export",
 };
 
 export default nextConfig;
